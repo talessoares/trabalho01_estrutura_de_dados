39a40a59abe094cba45621c5646be9500b49be88
Ricardo Costa <ricardo_hentges@hotmail.com>
2023-04-20 00:40:00 -0300

    modificada a interface

--- trabalho01/src/main/java/com/teste/lde/ILde.java
+++ trabalho01/src/main/java/com/teste/lde/ILde.java
@@ -2,6 +2,19 @@ package com.teste.lde;
 
 public interface ILde {
 
-    //interface da lista duplamente encadeada
+    public void insereInicio(Object info);
+    public void insereFim(Object info);
+    public boolean estahVazia();
+    public boolean remove(Object info);
+    public int tamanho();
+
+    // Retorna uma String que representa a lista a partir do inicio até o fim
+    public String getListaInicio();
+
+    // Retorna uma String que representa a lista a partir do fim até o inicio
+    public String getListaFim();
+
+    // Retorna o Noh que contém o objeto info
+    public Noh consultarInfo(Object info);
     
 }
